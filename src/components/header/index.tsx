1c78825ace5eed29333f50c17a2ae4272cbab4a8
matheus.amaral <matheus.amaral@aglets.com.br>
2024-02-06 08:57:18 -0300

    feat: criando o header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,118 @@
-export const Header = () => {
-    return (
-        <h1>Header</h1>
-    )
+import { useState } from 'react';
+import { Dialog, Popover } from '@headlessui/react';
+import { FaBarsStaggered } from "react-icons/fa6";
+import { FaUserCircle } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
+import { FiLogOut } from "react-icons/fi";
+import { Link } from 'react-router-dom';
+
+export function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [auth, setAuth] = useState(true);
+
+  return (
+    <header className="bg-white">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <Link to={"/"} className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+          </Link>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <FaBarsStaggered className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <Popover.Group className="hidden lg:flex lg:gap-x-12">
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Features
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Marketplace
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Company
+          </a>
+        </Popover.Group>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+           {
+            auth ? (
+                 <Link to={"/login"} className='flex gap-2 items-center'>
+                    <FaUserCircle fontSize={23}/>
+                </Link>
+            ) : (
+                <a className='hidden flex gap-2 items-center'>
+                    <Link to={"/dashboard"}>
+                        <FaUserCircle fontSize={23}/>
+                    </Link>
+                    <FiLogOut fontSize={23}/>
+                </a>
+             )
+           }
+        </div>
+      </nav>
+      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt=""
+              />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <IoMdClose className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Marketplace
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Company
+                </a>
+              </div>
+              <div className="py-6">
+                {
+                    !   auth ? (
+                        <a className='flex gap-2 items-center'>
+                            <span>Olá, Matheus!</span>
+                            <FiLogOut fontSize={23}/>
+                        </a>
+                    ) : (
+                        <a className='flex gap-2 items-center'>
+                            <FaUserCircle fontSize={23}/>
+                            <span>Entrar</span>
+                        </a>
+                    )
+                }
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  )
 }
